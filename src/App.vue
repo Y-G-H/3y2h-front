46cd09afffa940a40ef18a8a0701f752b94ac58c
wb-yanganghui <wb-yanganghui@cai-inc.com>
2021-03-20 20:29:47 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,45 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <a-layout id="components-layout-demo-top" class="layout">
+    <a-layout-header>
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :default-selected-keys="['2']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          nav 1
+        </a-menu-item>
+        <a-menu-item key="2">
+          nav 2
+        </a-menu-item>
+        <a-menu-item key="3">
+          nav 3
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
+      </a-breadcrumb>
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        <router-view/>
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      3y2h ©2021 Created by ygh,yzh,ylf,hk,hzj
+    </a-layout-footer>
+  </a-layout>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#components-layout-demo-top .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
 }
 </style>
---
+++ src/page/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="height: 400px">a</div>
+</template>
+
+<script>
+export default {
+name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
