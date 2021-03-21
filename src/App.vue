13c23f356eab3ca44a70416ccea244c53761d92d
wb-yanganghui <wb-yanganghui@cai-inc.com>
2021-03-21 21:46:02 +0800

    权限校验

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
           :default-selected-keys="['2']"
           :style="{ lineHeight: '64px' }"
         >
-          <a-menu-item v-for="num in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]" :key="num">
+          <a-menu-item v-for="num in [1,2,3,4,5,6,7,8,9,10]" :key="num">
             {{num}}
           </a-menu-item>
         </a-menu>
